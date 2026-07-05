d4ec60b91cdd603697b14c0f1b0756c475ef8129
yeonghwan.dev <yeonghwan.dev@gmail.com>
2026-07-05 17:47:32 +0900

    fix: 로고 크기 200px로 확대

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
         transition={{ duration: 0.5 }}
         className="mb-12"
       >
-        <Image src="/kinoby.png" alt="Kinoby" width={120} height={40} className="object-contain" />
+        <Image src="/kinoby.png" alt="Kinoby" width={200} height={67} className="object-contain" />
       </motion.div>
 
       {/* Pain 배지 */}
