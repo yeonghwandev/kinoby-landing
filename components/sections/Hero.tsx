9c67d282dff9c9ff2a660c87924fd830c7fc9cbb
yeonghwan.dev <yeonghwan.dev@gmail.com>
2026-07-05 18:22:45 +0900

    fix: 히어로 스크롤 힌트 화살표 제거

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -85,22 +85,6 @@ export default function Hero() {
         </motion.a>
       </motion.div>
 
-      {/* 스크롤 힌트 */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.2, duration: 0.8 }}
-        className="absolute bottom-10 left-1/2 -translate-x-1/2"
-      >
-        <motion.div
-          animate={{ y: [0, 8, 0] }}
-          transition={{ repeat: Infinity, duration: 1.6, ease: "easeInOut" }}
-          className="text-2xl"
-          style={{ color: "#C7D9F8" }}
-        >
-          ↓
-        </motion.div>
-      </motion.div>
     </section>
   )
 }
